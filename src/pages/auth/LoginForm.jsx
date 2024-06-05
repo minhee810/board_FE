137ba0d6bc2b75ae3ca0a8c8000555fc7dec5225
minhe2810 <minhe2810@naver.com>
2024-06-05 10:30:32 +0900

    feat : 사용자 아이디 기억 rememberId 기능 구현 완료

--- src/pages/auth/LoginForm.jsx
+++ src/pages/auth/LoginForm.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
 import api from "../../utils/api";
+import { useNavigate } from "react-router-dom";
 
 const LoginForm = () => {
   const [saveIDFlag, setSaveIDFlag] = useState(false);
   const LS_KEY_ID = "LS_KEY_ID";
   const LS_KEY_SAVE_ID_FLAG = "LS_KEY_SAVE_ID_FLAG";
+  const navigate = useNavigate();
+
   const [formData, setFormData] = useState({
     email: "",
     password: "",
@@ -21,7 +24,7 @@ const LoginForm = () => {
   const handleSubmit = async (e) => {
     console.log("handleSubmit() 호출");
     e.preventDefault();
-
+    console.log("email : ", formData.email);
     if (formData.email === "") {
       alert("이메일을 입력해주세요.");
       return false;
@@ -37,24 +40,41 @@ const LoginForm = () => {
 
     try {
       const response = await api.post(`/api/login`, formData);
-      console.log("formData : ", formData);
+      navigate("/");
     } catch (error) {
       console.log("error.status :", error.response.status);
     }
   };
 
+  // 아이디 저장 체크박스 활성화
   const handleRememberId = () => {
+    console.log("check 발생");
+    // 로컬스토리지에 LS_KEY_SAVE_ID_FLAG 라는 키값으로 아이디 기억 버튼의 상태를 저장
+    // 체크 ->  true 언체크 -> false
+    // 기본 값이 false
     localStorage.setItem(LS_KEY_SAVE_ID_FLAG, !saveIDFlag);
+    // 상태 변경해서 저장
     setSaveIDFlag(!saveIDFlag);
   };
 
+  // 컴포넌트가 처음 마운트 될 때만 실행되도록 빈 배열을 두번째 인자로 넘김
   useEffect(() => {
+    // 로컬스토리지에서 저장 활성화 버튼 여부
     let idFlag = JSON.parse(localStorage.getItem(LS_KEY_SAVE_ID_FLAG));
+    // 저장 여부가 없을 경우 현재의 저장 여부를 저장
     if (idFlag !== null) setSaveIDFlag(idFlag);
+
+    // 저장이 안되어 false일 경우 ""로 저장
     if (idFlag === false) localStorage.setItem(LS_KEY_ID, "");
+
+    // storage 에서 꺼낸 값 id 값
     let data = localStorage.getItem(LS_KEY_ID);
-    if (data !== null) setLoginId(data);
-  });
+
+    // 저장되어있는 값이 있을 경우
+    // 데이터가 null이 아닐 경우
+    if (data !== null) setFormData({ email: data });
+  }, []);
+
   return (
     <div className="bg-gradient-primary">
       <div className="container">
@@ -80,6 +100,7 @@ const LoginForm = () => {
                             className="form-control form-control-user"
                             aria-describedby="emailHelp"
                             placeholder="Enter Email Address..."
+                            value={formData.email}
                           />
                         </div>
                         <div className="form-group">
@@ -95,7 +116,7 @@ const LoginForm = () => {
                         <div className="form-group">
                           <div className="custom-control custom-checkbox small">
                             <input
-                              onClick={handleRememberId}
+                              onChange={handleRememberId}
                               type="checkbox"
                               checked={saveIDFlag}
                               className="custom-control-input"
--- src/utils/api.js
+++ src/utils/api.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const api = axios.create({
   headers: { "Content-Type": "application/json" },
+  withCredentials: true, // 다른 도메인에 요청을 보낼 때 요청에 인증 정보를 담아서 보낼 지 결정하는 항목
+  // 쿠키나 인증 헤더 정보를 포함시켜 요청하고 싶다면 클라이언트에서 api 요청 메서드를 보낼 때 true로 설정해서 보내야함.
+  // 서버에서도 Access-Control-Allow-Credentials 헤더를 true로 설정해서 보내야함.
 });
 
 // request 인터셉터
