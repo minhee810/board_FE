d14cd912bee8bfc12afb596040732d429b265be9
minhee <minhee810@naver.com>
2024-06-16 20:37:08 +0900

    feat : commentList 부분에서 댓글 정보 관리하는 state 변수 [] 빈 배열로 초기화 하도록 수정

--- src/components/CommentList.jsx
+++ src/components/CommentList.jsx
@@ -7,7 +7,7 @@ import { dateFormat } from "../utils/utility";
 import CommentWrite from "./CommentWrite";
 
 const CommentList = ({ boardId }) => {
-  const [cList, setCList] = useState();
+  const [cList, setCList] = useState([]);
   const [modifyMode, setModifyMode] = useState(false);
   const [modifyId, setModifyId] = useState("");
 
@@ -15,28 +15,24 @@ const CommentList = ({ boardId }) => {
     getList(boardId);
   }, [boardId]);
 
-  // useEffect(() => {
-  //   getList(boardId);
-  // }, []);
-
-  useEffect(() => {}, []);
   async function getList(boardId) {
     const response = await getCommentList(boardId);
-    setCList(response.data);
+    setCList((prevCList) => response.data);
   }
 
   const handleCommentDelete = async (commentId) => {
     if (window.confirm("댓글을 삭제하시겠습니까? ")) {
       const response = await deleteComment(boardId, commentId);
-      console.log(response);
+      console.log("response : ", response);
+      // comment.commentId 가 파라미터와 일치하지 않는 원소만 추출해서 새로운 배열을 생성
+      setCList(cList.filter((comment) => comment.commentId !== commentId));
     }
   };
 
   // 댓글 저장
   const handleSubmit = async (data) => {
     console.log("handleSubmit 호출: ");
-    // setCList((prevCList) => [...prevCList, { ...data, principal: 1 }]);
-    setCList([...cList, data]);
+    setCList((preveCList) => [...cList, data]);
   };
 
   // 댓글 수정
@@ -47,22 +43,13 @@ const CommentList = ({ boardId }) => {
       setModifyMode(!modifyMode);
     }
     setCList((prevComments) =>
-    // 댓글 리스트에 새로 추가된 댓글의 내용을 추가할 경우 
-    // 기존의 댓글 배열에서 내가 수정한 댓글 아이디와 같은 댓글의 아이디를 비교해서 동일하다면 댓글의 내용을 수정한 내용으로 변경하고, 
-    // 아니면 기존의 댓글 내용 유지
+      // 댓글 리스트에 새로 추가된 댓글의 내용을 추가할 경우
+      // 기존의 댓글 배열에서 내가 수정한 댓글 아이디와 같은 댓글의 아이디를 비교해서 동일하다면 댓글의 내용을 수정한 내용으로 변경하고,
+      // 아니면 기존의 댓글 내용 유지
       prevComments.map((comment) =>
         comment.id === updatedComment.id ? updatedComment : comment
       )
     );
-
-    // 해당 댓글의 아이디를 가져와서 그 내용으로 변경하기!
-
-    // 댓글정보를 가져와서 content 내용으로 넣는다?
-
-    // 댓글 수정 버튼을 클릭하면 부모 컴포넌트로 데이터를 넘긴다.
-    // 넘겨 받은 부모 커모넌트가 기존의 댓글 정보에 새로 응답 받은 데이터를 추가하면?
-    // 밑으로 댓글이 쌓이는 문제점이 발생한다.
-    // 따라서 데이터를 추가해주는 방식이 아닌 데이터를 업데이트 시켜주는? 기능을 만들어야 한다.
   };
 
   // 수정할 게시글의 아이디 값을 받아온다.
