e12af7dd44c56c536db9b79bdcd43c031d395c9d
minhe2810 <minhe2810@naver.com>
2024-06-25 14:47:40 +0900

    fix : usernameInput replace 함수 , onBlur 이벤트 선언

--- src/components/auth/UsernameInput.jsx
+++ src/components/auth/UsernameInput.jsx
@@ -1,22 +1,41 @@
-import React from "react";
-import { InputField } from "../common/input/InputField";
+import React, { forwardRef, memo, useState } from "react";
+import InputField from "../common/input/InputField";
+import { REPLACE_VALID, regTest } from "../../utils/validation";
+import { hintMsg } from "../../utils/message";
 
-export const UsernameInput = React.memo(({ value, onChange, onBlur }) => {
+const UsernameInput = forwardRef(({ value, onChange, onBlur }, ref) => {
+  const [username, setUsername] = useState("");
+  const [validText, setValidText] = useState("");
+
   const handleChange = (event) => {
-    console.log("username input chanege value");
+    const { name, value } = event.target;
+    const filteredValue = value.replace(REPLACE_VALID[name], "");
+    setUsername(filteredValue);
     onChange(event);
   };
+
+  const handleBlur = (event) => {
+    if (onBlur) {
+      onBlur(event, username);
+    }
+  };
+
   return (
-    <InputField
-      data-name="사용자 이름"
-      data-check={true}
-      name="username"
-      className="form-control form-control-user"
-      value={value}
-      maxLength={10}
-      onBlur={onBlur}
-      onChange={handleChange}
-      placeholder="이름"
-    />
+    <>
+      <InputField
+        data-name="사용자 이름"
+        data-check={true}
+        name="username"
+        className="form-control form-control-user"
+        value={username}
+        maxLength={10}
+        onBlur={handleBlur}
+        onChange={handleChange}
+        placeholder="이름"
+        ref={ref}
+      />
+    </>
   );
 });
+
+export default memo(UsernameInput);
